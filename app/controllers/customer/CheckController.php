4b75de79708d1a3d8d4f8fa9430cfdd9bad5743a
Salon-sai <coke1231078@gmail.com>
2015-01-14 20:39:18 +0800

    wx

--- app/controllers/customer/CheckController.php
+++ app/controllers/customer/CheckController.php
@@ -18,9 +18,9 @@ class CheckController extends \BaseController {
 	 */
 	public function index()
 	{
-				// $postStr 	= file_get_contents("php://input");
+		$postStr 	= file_get_contents("php://input");
 		// $postStr	= $GLOBALS["HTTP_RAW_POST_DATA"];
-		$postStr	= Input::get('HTTP_RAW_POST_DATA');
+		// $postStr	= Input::get('HTTP_RAW_POST_DATA');
 
 		if(!empty($postStr)){
 			libxml_disable_entity_loader(true);
