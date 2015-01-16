19ec998e50799e59c9d7ca614261d6a428ead951
Salon-sai <coke1231078@gmail.com>
2015-01-16 11:51:20 +0800

    wx

--- app/controllers/customer/CheckController.php
+++ app/controllers/customer/CheckController.php
@@ -237,7 +237,7 @@ class CheckController extends \BaseController {
 		$PicUrl 	= "http://104.237.155.177/pic/TestDemo.jpg";
 		$Url 		= "https://open.weixin.qq.com/connect/oauth2/authorize?
 						appid=wx93711e54bf475da8&
-						redirect_uri=http://104.237.155.177/oauth2.php&
+						redirect_uri=http%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&
 						response_type=code&
 						scope=snsapi_userinfo#wechat_redirect";
 		// $Url 		= "https://open.weixin.qq.com/connect/oauth2/authorize?
