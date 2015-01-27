f32c2bd7ee543e81b9ace5fd13a8ce96ef5e1c63
Salon-sai <coke1231078@gmail.com>
2015-01-27 14:44:13 +0800

    wx

--- app/controllers/customer/RestaurantController.php
+++ app/controllers/customer/RestaurantController.php
@@ -121,9 +121,9 @@ class RestaurantController extends BaseController {
 		$radLat = deg2rad($latY);
 
 		$descriptions 	= DB::table('descriptions')
-			->whereRaw("( ? * 2 * ASIN( SQRT( POW( SIN( (RADIANS(locationX) - ?) / 2),2) + 
-				COS(locationY) * COS(?) * POW( SIN( (RADIANS(locationY) - ?) / 2),2))) ) < scale", 
-			array(EARTH_RADIUS, $radLng, $latY, $radLat))->get();
+			->whereRaw("( ? * 2 * ASIN( SQRT( POW( SIN( (RADIANS(locationX) - RADIANS(?)) / 2),2) + 
+				COS(locationY) * COS(?) * POW( SIN( (RADIANS(locationY) - RADIANS(?)) / 2),2))) ) < scale", 
+			array(EARTH_RADIUS, $lngX, $latY, $latY))->get();
 		return View::make('customer.restaurant.index')
 			->with('descriptions', $descriptions);
 	}
