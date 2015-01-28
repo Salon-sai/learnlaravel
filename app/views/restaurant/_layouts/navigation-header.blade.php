454699701f5e374628bd14393cd73bba00516ceb
Salon-sai <coke1231078@gmail.com>
2015-01-28 23:58:24 +0800

    wx

--- app/views/restaurant/_layouts/navigation-header.blade.php
+++ app/views/restaurant/_layouts/navigation-header.blade.php
@@ -7,10 +7,12 @@
 				<li><a href="{{URL::route('logout')}}">Logout</a></li>
 				<li>
 					<form class="navbar-form navbar-left">
-						@if(Sentry::getUser()->description->status)
-							<input data-on-color="primary" data-off-color="danger" data-on-text="Open" data-off-text="Close" type="checkbox" name="restaurant_status" checked />
-						@else
-							<input data-on-color="primary" data-off-color="danger" data-on-text="Open" data-off-text="Close" type="checkbox" name="restaurant_status" />
+						@if(Sentry::getUser()->description)
+							@if(Sentry::getUser()->description->status)
+								<input data-on-color="primary" data-off-color="danger" data-on-text="Open" data-off-text="Close" type="checkbox" name="restaurant_status" checked />
+							@else
+								<input data-on-color="primary" data-off-color="danger" data-on-text="Open" data-off-text="Close" type="checkbox" name="restaurant_status" />
+							@endif
 						@endif
 					</form>
 				</li>
