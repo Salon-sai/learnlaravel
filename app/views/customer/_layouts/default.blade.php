026e336a389b58292b2cd0ec68dc9cd7e612ed72
Salon-sai <coke1231078@gmail.com>
2015-01-30 15:23:27 +0800

    wx

--- app/views/customer/_layouts/default.blade.php
+++ app/views/customer/_layouts/default.blade.php
@@ -8,7 +8,7 @@
 	<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 	<script type="text/javascript">
 		wx.config({
-			debug: false,
+			debug: true,
 			appId: "$signPackage['appId']",
 			timestamp: "$signPackage['timestamp']",
 			nonceStr: "$signPackage['nonceStr']",
