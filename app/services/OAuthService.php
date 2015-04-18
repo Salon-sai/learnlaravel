49846040b4511d051014de113295579f5d26c5b1
Salon-sai <coke1231078@gmail.com>
2015-04-18 12:26:11 +0800

    fix the access token with cache

--- app/services/OAuthService.php
+++ app/services/OAuthService.php
@@ -1,5 +1,7 @@
 <?php
 
+use Cache;
+
 define('APPID', 'wx6b67feeba41a14f3');
 define('SECRET', '29943ae5d7b778c9761961156baf5e31');
 define('GRANT_TYPE', 'authorization_code');
@@ -9,6 +11,11 @@ class OAuthService {
 
 	private function getAccessToken(){
 		$code = Input::get('code');
+		$oauth_config = Cache::get("OAUTH_CONFIG");
+		if ($oauth_config)
+		{
+			return $oauth_config;
+		}
 		if($code){
 			try{
 				$data 	= array(
@@ -20,6 +27,7 @@ class OAuthService {
 				$sender = new Sender;
 				Log::info('ready to get url message');
 				$result = $sender->httpSend(ACCESS_TOKEN_OAuth_URL, true, $data);
+				Cache::put('OAUTH_CONFIG', $result, 120);
 				Log::info('success to get ACCESS_TOKEN');
 				return $result;
 			}catch(\Exception $e){
